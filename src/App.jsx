6347ac2c98ced2001244b6941609f7b251847381
Dharshini8703 <dharshinichakra2003@gmail.com>
2025-03-10 20:32:55 +0530

    About component and headings are given

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import DestinationDetail from './pages/DestinationDetail';
 import { DestinationsProvider } from './context/DestinationsContext';
+import About from './pages/About';
 
 function App() {
   return (
@@ -13,10 +14,22 @@ function App() {
           <Sidebar />
           <div className="flex-1 md:ml-64"> {/* Add margin to offset sidebar width */}
             <main className="p-6 overflow-y-auto">
-              <Routes>
+            <div className="max-w-7xl mx-auto">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-gray-800 mb-2">
+          { 'Explore Tamil Nadu'}
+        </h1>
+        <p className="text-gray-600">
+          Discover the beauty, culture, and heritage of Tamil Nadu
+        </p>
+      </div>
+      <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/destination/:id" element={<DestinationDetail />} />
+                <Route path='/about' element={ <About /> } />
               </Routes>
+    </div> 
+             
             </main>
           </div>
         </div>
---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+  return (
+    // <div className="max-w-7xl mx-auto">
+    //   <div className="mb-8">
+    //     <h1 className="text-3xl font-bold text-gray-800 mb-2">
+    //       { 'Explore Tamil Nadu'}
+    //     </h1>
+    //     <p className="text-gray-600">
+    //       Discover the beauty, culture, and heritage of Tamil Nadu
+    //     </p>
+    //   </div>
+
+    // </div>  
+    <div>About</div>
+    )
+}
+
+export default About
